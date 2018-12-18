2ae848f368177a584705fcbd7cdc1781ecb4baef
Alexandre Pinho <lxpnh98@gmail.com>
2018-12-18 17:06:48 +0000

    Handle auction timestamps, return bill to charge old client.

--- server-allocator/src/serverallocator/Auction.java
+++ server-allocator/src/serverallocator/Auction.java
@@ -13,6 +13,7 @@ class Bid {
         this.value = v;
     }
 
+    // bid identified by name of client
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Bid)) return false;
@@ -20,6 +21,7 @@ class Bid {
     }
 }
 
+// sort by bid value
 class BidComparator implements Comparator<Bid> {
     public int compare(Bid o1, Bid o2) {
         return Float.compare(o1.value, o2.value);
@@ -30,39 +32,59 @@ public class Auction {
 
     private ServerProduct serverToRent;
     private float minPrice;
+    private float currPrice;
     private TreeSet<Bid> bids;
     private String clientUsername;
+    private long timestamp;
 
     public Auction (ServerProduct serverToRent, float minPrice) throws IOException {
 
         this.serverToRent = serverToRent;
         this.minPrice = minPrice;
         this.clientUsername = null;
-        this.bids = new TreeSet(new BidComparator());
+        this.bids = new TreeSet<>(new BidComparator());
 
     }
 
     // o que acontece quando o leilão recebe uma oferta
-    private void makeBid(String clientUsername, String money) throws IOException {
+    // returns bill to charge old client, or null
+    private Bill makeBid(String clientUsername, String money) throws IOException {
 
         float price = Float.parseFloat(money);
+        String oldClient = this.clientUsername;
+        float toCharge = 0.0f;
 
         if (price >= this.minPrice) {
 
             // add new bid or update client's bid
             this.bids.add(new Bid(clientUsername, price));
 
-            String newClient = this.bids.last().client;
+            Bid bestBid = this.bids.last();
+            String newClient = bestBid.client;
             if (this.clientUsername != newClient) {
-                 // TODO: timestamps
+
+                // 
+                long newTimestamp = System.currentTimeMillis();
+
                 // calculate amount to charge old client
+                if (this.clientUsername != null) {
+                    long timeElapsed = newTimestamp - this.timestamp;
+                    toCharge = timeElapsed * this.currPrice;
+                }
 
                 // change current client
                 this.clientUsername = newClient;
+                this.currPrice = bestBid.value;
+                this.timestamp = newTimestamp;
                 
             }
 
         }
+
+        if (toCharge > 0.0) {
+            return new Bill(oldClient, toCharge);
+        }
+        return null;
 
     }
 
