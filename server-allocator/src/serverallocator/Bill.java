9cfa37529c1e2903415ba423b06fa83899faa838
Alexandre Pinho <lxpnh98@gmail.com>
2018-12-18 17:06:48 +0000

    Handle auction timestamps, return bill to charge old client.

---
+++ server-allocator/src/serverallocator/Bill.java
@@ -0,0 +1,11 @@
+package serverallocator;
+
+public class Bill {
+    String client;
+    float value;
+
+    public Bill(String client, float value) {
+        this.client = client;
+        this.value = value;
+    }
+}
